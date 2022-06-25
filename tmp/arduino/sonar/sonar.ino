8e621c4d1a18da3633dd4481dd113d0b5f1d3721
alexbora <alexbora@gmail.com>
2022-06-25 08:12:15 +0300

    T460 - sâmbătă 25 iunie 2022, 08:12:15 +0300

--- tmp/arduino/sonar/sonar.ino
+++ tmp/arduino/sonar/sonar.ino
@@ -11,18 +11,46 @@
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
 void setup() {
-  Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
   pinMode(13, OUTPUT);
+  Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
+  
 }
 
+void fnull(){}
+
 void loop() {
-  delay(50); //50                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+  delay(100); //50                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
   Serial.print("Ping: ");
   //digitalWrite(13, HIGH);
   Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
   Serial.println("cm");
-  if(sonar.ping_cm() < 10)
-  digitalWrite(13, HIGH);
-  else
-  digitalWrite(13, LOW);
+
+int cm = sonar.ping_cm();  
+
+#if 0
+switch (cm) {
+	case 0: break;
+	case 1 ... 10: digitalWrite(13, HIGH); break;
+	default: digitalWrite(13, LOW); break; 
+		
 }
+
+#endif
+void (*fp)(int, int) = digitalWrite;
+
+if (cm == 0) fnull();
+
+if(16 > cm && cm > 1) 
+  digitalWrite(13, HIGH);
+else
+  digitalWrite(13, LOW);
+
+#if 0
+  if(sonar.ping_cm() < 10 && sonar.ping_cm() > 1)
+  	digitalWrite(13, HIGH);
+  if (sonar.ping_cm() == 0) fnull();  
+  else
+  	digitalWrite(13, LOW);
+#endif
+}
+
