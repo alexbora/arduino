2755f1a947ca2f811c3cafbcf48e526c5fc11f60
alexbora <alexbora@gmail.com>
2022-06-25 15:33:36 +0300

    T460 - sâmbătă 25 iunie 2022, 15:33:36 +0300

--- tmp/arduino/sonar/sonar.ino
+++ tmp/arduino/sonar/sonar.ino
@@ -6,26 +6,110 @@
 
 #define TRIGGER_PIN  8  // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define ECHO_PIN     7  // Arduino pin tied to echo pin on the ultrasonic sensor.
-#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+#define MAX_DISTANCE 400 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
+
+int EN_A = 9;
+int in1 = 2; // stanga 
+int in2 = 3;
+
+int EN_B = 10;
+int in3 = 4; // dreapta
+int in4 = 5;
 
 void setup() {
   pinMode(13, OUTPUT);
   Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
+
+
+/* pinMode(EN_A, OUTPUT); */
+/* pinMode(in1, OUTPUT); */
+/* pinMode(in2, OUTPUT); */
+/* pinMode(in3, OUTPUT); */
+/* pinMode(in4, OUTPUT); */
+/* pinMode(EN_B, OUTPUT); */
+/* analogWrite(EN_A, 255); */
+/* analogWrite(EN_B, 255); */
+
   
 }
 
-void fnull(){}
+void fnull(){__asm__("nop");}
+
+void ahead(){
+digitalWrite(in1, HIGH); //stanga fata
+digitalWrite(in2, LOW);
+
+digitalWrite(in4, HIGH); // dreapta fata
+digitalWrite(in3, LOW);
+analogWrite(EN_B, 100);
+}
+
+void back(){
+digitalWrite(in1, LOW); //stanga fata
+digitalWrite(in2, HIGH);
+
+digitalWrite(in4, LOW); // dreapta fata
+digitalWrite(in3, HIGH);
+}
+
+void stop(){
+digitalWrite(in1, LOW); //stanga fata
+digitalWrite(in2, LOW);
+
+digitalWrite(in4, LOW); // dreapta fata
+digitalWrite(in3, LOW);
+delay(500);
+}
+
+
+void left(){
+digitalWrite(in1, LOW); //stanga fata
+digitalWrite(in2, HIGH);
+
+digitalWrite(in4, HIGH); // dreapta fata
+digitalWrite(in3, LOW);
+delay(500);
+}
+
+void right(){
+digitalWrite(in1, HIGH); //stanga fata
+digitalWrite(in2, LOW);
+
+digitalWrite(in4, LOW); // dreapta fata
+digitalWrite(in3, HIGH);
+delay(500);
+}
+
+void obstacle(void){
+stop();
+delay(50);
+void (*f[])(void) = {left, right};
+/* f[rand()%2](); */
+back();
+delay(2000);
+stop();
+}
+
 
 void loop() {
+  /* int potValue = analogRead(A0); // Read potentiometer value */
+  /* int out = map(potValue, 0, 1023, 0 , 255); // Map the potentiometer value from 0 to 255 */
+  /* analogWrite(enA, pwmOutput); */
+
+
+
   delay(100); //50                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+  
   Serial.print("Ping: ");
-  //digitalWrite(13, HIGH);
   Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
   Serial.println("cm");
 
+
 int cm = sonar.ping_cm();  
+/* oneSensorCycle(cm); */
 
 #if 0
 switch (cm) {
@@ -36,14 +120,19 @@ switch (cm) {
 }
 
 #endif
-void (*fp)(int, int) = digitalWrite;
+/* void (*fp)(int, int) = digitalWrite; */
 
-if (cm == 0) fnull();
+/* if (cm == 0) fnull(); */
 
-if(16 > cm && cm > 1) 
-  digitalWrite(13, HIGH);
+if(31 > cm && cm > 1) 
+/* PINB |= 1<<5; */
+	PORTB = 1 << 5, stop(), stop(), back();
+  /* digitalWrite(13, HIGH); */
+else if(cm == 0) fnull();
 else
-  digitalWrite(13, LOW);
+  /* digitalWrite(13, LOW); */
+	PORTB &= ~(1<<5), ahead();
+
 
 #if 0
   if(sonar.ping_cm() < 10 && sonar.ping_cm() > 1)
@@ -54,3 +143,15 @@ else
 #endif
 }
 
+
+void oneSensorCycle(int i) { // Sensor ping cycle complete, do something with the results.
+  // The following code would be replaced with your code that does something with the ping results.
+    Serial.print(i);
+    Serial.print("=");
+    Serial.print("cm ");
+    Serial.println();
+}
+
+void potentiometer(){
+  Serial.print("Potentiometer value: ");
+  Serial.print(analogRead(A0));}
